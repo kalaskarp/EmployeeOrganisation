24db4e9a7bbeb3f0e77b1b349f3961303893fd07
kalaskarp <kalaskarpratik22@gmail.com>
2025-07-24 19:29:16 +0530

    Added Caching

--- src/main/java/com/empdept/repository/DepartmentRepository.java
+++ src/main/java/com/empdept/repository/DepartmentRepository.java
@@ -1,8 +1,11 @@
 package com.empdept.repository;
 
-
 import com.empdept.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
+import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department, String> {
+    @EntityGraph(attributePaths = {"employees"})
+    List<Department> findAll();
 }
